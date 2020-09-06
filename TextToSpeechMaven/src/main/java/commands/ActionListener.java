c654ecdcd1e9c38f4547b474ba13a17cf7e68eeb
GiannisChouliaras <giannis@github.com>
2020-09-06 16:17:55 +0300

    removing

--- TextToSpeechMaven/src/main/java/commands/ActionListener.java
+++
@@ -1,8 +0,0 @@
-package commands;
-
-import java.awt.event.ActionEvent;
-
-public interface ActionListener extends java.awt.event.ActionListener {
-    public void actionPerformed(ActionEvent actionEvent);
-
-}
--- TextToSpeechMaven/src/main/java/commands/ChangeListener.java
+++
@@ -1,8 +0,0 @@
-package commands;
-
-import javax.swing.event.ChangeEvent;
-
-public interface ChangeListener extends javax.swing.event.ChangeListener {
-    public void stateChanged(ChangeEvent changeEvent);
-
-}
